50e2700867a6464638b4d843bda51adeb439d03d
System Administrator <root@MacBook-Pro.local>
2021-06-06 23:49:49 +1000

    final1

---
+++ src/componants/portfolioList/PortfolioList.jsx
@@ -0,0 +1,10 @@
+import './portfolioList.scss'
+
+export default function PortfolioList({title,active, setSelected, id}) {
+    return (
+        <li className={active? "portfolioList active" : "portfolioList"}
+        onClick={()=>setSelected(id)}>
+            {title}
+        </li>
+    )
+}
