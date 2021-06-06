47d70ef089d94ba19fd087b50f5c33f836e75048
Gursher Singh <gurshersingh@MacBook-Pro.local>
2021-06-06 22:28:36 +1000

    contact form finish

--- src/componants/contact/Contact.jsx
+++ src/componants/contact/Contact.jsx
@@ -3,7 +3,17 @@ import  './contact.scss'
 export default function Contact() {
     return (
         <div className = "contact" id="contact">
-            con
+            <div className="left">
+                <img src="assets/shake.svg" alt="" />
+            </div>
+            <div className="right">
+                <h2>Contact.</h2>
+                <form>
+                    <input type="text" placeholder="Email" />
+                    <textarea  placeholder="Message"></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
         </div>
     )
 }
